9aeb78e4061dac15022fd9f3ff9aeacdc12f565d
Asanda <noelmajola@gmail.com>
2022-08-06 19:12:14 +0200

    Replace link with Navlink

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavigationMenu = () => {
   return (
@@ -11,16 +11,16 @@ const NavigationMenu = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/Asanda-Majola/">
+            <Nav.Link as={NavLink} to="/Asanda-Majola/">
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="projects">
+            <Nav.Link as={NavLink} to="projects">
               Projects
             </Nav.Link>
-            <Nav.Link as={Link} to="blogs">
+            <Nav.Link as={NavLink} to="blogs">
               Blogs
             </Nav.Link>
-            <Nav.Link as={Link} to="contact">
+            <Nav.Link as={NavLink} to="contact">
               Contact
             </Nav.Link>
           </Nav>
