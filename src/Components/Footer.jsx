cee08bd4fa12e7621d88825a5e3069d9f56ae9fd
Asanda <noelmajola@gmail.com>
2022-08-27 00:34:08 +0200

    Remove github and linkedIn social media icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,32 +1,9 @@
 import Container from "react-bootstrap/esm/Container";
-import { Github } from "react-bootstrap-icons";
-import { Linkedin } from "react-bootstrap-icons";
 
 const Footer = () => {
   return (
     <Container style={{ paddingTop: "15px" }}>
-      <footer>
-        &copy; Asanda Majola 2022{" "}
-        <span style={{ marginLeft: "10px" }}>
-          <a
-            href="https://github.com/AsandahDevs"
-            target="_blank"
-            rel="noreferrer"
-            className="anchor-style"
-          >
-            <Github /> Github
-          </a>{" "}
-          |{" "}
-          <a
-            href="https://www.linkedin.com/in/asanda-majola-738067186/"
-            target="_blank"
-            rel="noreferrer"
-            className="anchor-style"
-          >
-            <Linkedin /> Linkedin
-          </a>
-        </span>
-      </footer>
+      <footer>&copy; Asanda Majola 2022 </footer>
     </Container>
   );
 };
